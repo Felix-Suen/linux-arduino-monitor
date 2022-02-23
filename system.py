72f28120262429ad26b42b0e676f8620a6e28569
Felix-Suen <felix.suen2000@gmail.com>
2022-02-22 23:27:45 -0800

    linux server external monitor

---
+++ system.py
@@ -0,0 +1,49 @@
+import psutil
+import serial
+import time
+import os
+ser = serial.Serial('/dev/ttyUSB0') #define serial port.
+line = ""
+while True:
+    sep = ',' 
+    sep1 = '.'
+    t = os.popen('uptime -p').read()[:-1] #open the file /proc/uptime and read it
+    up = str(t)
+    
+    cpu = psutil.cpu_percent(interval=10) #reads the cpu percentage
+    cpu = str(cpu)
+    cpu = str(cpu).replace('"', "")
+    cpu = str(cpu).replace("'", "")
+    cpu = str(cpu).replace(' ', "")
+    cpu = 'CPU ' + cpu
+    cpu = str(cpu) + '%'
+    
+    amount_of_ram = str(psutil.virtual_memory()[2]).split(sep1, 1)[0]
+    ram = 'RAM', str(amount_of_ram)
+    ram = str(ram) + "%"
+
+    disk = 'DISK ' + str(psutil.disk_usage('/')[3]) + '%'
+
+    up = up.replace("(", "") #replace all unecessary characters from the strings
+    up = up.replace(",", "")
+    up = up.replace("'", "")
+    up = up.replace(")", "")
+    up = up.replace("up", "UP")
+    up = up.replace(" minutes", "m")
+    up = up.replace(" minute", "m")
+    up = up.replace(" hours", "h")
+    up = up.replace(" hour", "h")
+    up = up.replace(" days", "d")
+    up = up.replace(" day", "d")
+    ram = ram.replace("(", "")
+    ram = ram.replace(")", "")
+    ram = ram.replace(",", "")
+    ram = ram.replace("'", "")
+
+    combined = up + "\n\n" + cpu + "\n\n" + ram + "\n\n" + disk
+    print(combined)
+
+    ser.write(combined.encode()) # Write the data 
+    line = ser.readline() # Read the serial output
+    time.sleep(2) # Wait two seconds
+
