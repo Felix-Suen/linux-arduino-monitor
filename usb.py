49a5e8d7c62f6d7976cfa78a994747a046b42033
Felix-Suen <felix.suen2000@gmail.com>
2022-02-24 10:23:52 -0800

    vendor id

--- usb.py
+++ usb.py
@@ -3,6 +3,10 @@
 import pyudev
 import os
 
+# change this value to your specific arduino board #
+# run usb-devices command to find that information #
+vendor = '1a86'
+
 def main():
     path = os.path.dirname(os.path.realpath(__file__))
     # runs as a daemon to check if the arduino is connected via usb tty
@@ -12,7 +16,7 @@ def main():
     for action, device in monitor:
         vendor_id = device.get('ID_VENDOR_ID')
         # get the vendor id by running the command "lsusb"
-        if (action == 'add' and vendor_id == '1a86'):
+        if (action == 'add' and vendor_id == vendor):
             print("device is connected")
             os.system(path + '/system.py') # system.py is in the same directory
 
